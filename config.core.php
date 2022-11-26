fc447a3679dcec0a084c75acbbe2ce7a918e8fbb
admin <liza.aksenova.98@mail.ru>
2022-11-26 18:11:25 +0700

    fix: Fix config file

--- config.core.php
+++ config.core.php
@@ -1,9 +1,4 @@
 <?php
-/*
- * This file is managed by the installation process.  Any modifications to it may get overwritten.
- * Add customizations to the $config_options array in `core/config/config.inc.php`.
- *
- */
 define('MODX_CORE_PATH', 'D:\OpenServer\domains\test-modx/core/');
 define('MODX_CONFIG_KEY', 'config');
 ?>
